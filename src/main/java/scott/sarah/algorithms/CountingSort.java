8f93868ebc376f79c3159f5b3a3f30b4a6ca3d78
Sarah Scott <s.j.scott84@gmail.com>
2017-03-30 14:58:59 -0700

    Add counting sort

---
+++ src/main/java/scott/sarah/algorithms/CountingSort.java
@@ -0,0 +1,72 @@
+package scott.sarah.algorithms;
+
+public class CountingSort {
+	
+	int[] array;
+	int[] countArray;
+	int[] resultArray;
+	
+	public CountingSort(int[] array, int highNumber){
+		this.array = array;
+		this.countArray = new int[highNumber + 1];
+		this.resultArray = new int[array.length];
+	}
+
+	public int[] countingSort (){
+		countArray = addZeroToCountArray(countArray);
+		
+		countArray = countHowManyValuesInArray(countArray);
+		
+		countArray = countAllValuesTogether(countArray);
+
+		for(int i = 0; i < array.length; i++){
+			resultArray[countArray[array[i]]-1] = array[i];
+			countArray[array[i]]--;
+		}
+		
+		return resultArray;
+	}
+	
+	/**
+	 * Ensures all elements in countArray are zero
+	 * @param zeroArray
+	 * @return
+	 */
+	int[] addZeroToCountArray(int[] zeroArray){
+		for (int i=0; i<array.length; i++){
+            zeroArray[i] = 0;
+		}
+		
+		return zeroArray;
+	}
+	
+	/**
+	 * Counts how many times a value appears in the array to sort
+	 * and adds this count to countArray (e.g. array to sort is {3,1,3}
+	 * countArray would be {0,1,0,2}
+	 * @param arrayToCount
+	 * @return
+	 */
+	int[] countHowManyValuesInArray(int[] arrayToCount){
+		for (int i = 0; i < array.length; i++){
+			int x = arrayToCount[array[i]];
+			x++;
+			arrayToCount[array[i]] = x;
+		}
+		return arrayToCount;
+	}
+	
+	/**
+	 * Adds up countArray(e.g. if countArray is {0,1,0,2} as above this method
+	 * would transform countArray to {0,1,1,3}
+	 * @param arrayToCount
+	 * @return
+	 */
+	int[] countAllValuesTogether(int[] arrayToCount){
+		for(int i = 1; i < arrayToCount.length; i++){
+			arrayToCount[i] = arrayToCount[i] + arrayToCount[i-1];
+		}
+		return arrayToCount;
+	}
+
+}
---
+++ src/test/java/scott/sarah/algorithms/CountingSortTest.java
@@ -0,0 +1,54 @@
+package scott.sarah.algorithms;
+
+import static org.junit.Assert.assertArrayEquals;
+
+import org.junit.Test;
+
+public class CountingSortTest {
+	@Test
+	public void addZeroToCountArrayShouldMakeAllValuesZero(){
+		int[] array = {3,1,2};
+		int[] expectedArray = {0,0,0};
+		CountingSort countingSort = new CountingSort(array, 3);
+		
+		int[] countArray = countingSort.addZeroToCountArray(array);
+		
+		assertArrayEquals(countArray, expectedArray);
+	}
+	
+	@Test
+	public void shouldCountHowManyTimesAValueIsInArrayAndUpdateCountArray(){
+		int[] array = {3,1,1};
+		int[] emptyCountArray = {0,0,0,0};
+		int[] expectedArray = {0,2,0,1};
+		CountingSort countingSort = new CountingSort(array, 3);
+		
+		int[] countArray = countingSort.countHowManyValuesInArray(emptyCountArray);
+		
+		assertArrayEquals(countArray, expectedArray);
+	}
+	
+	@Test
+	public void shouldAddUpAllValuesInCountArray(){
+		int[] array = {3,1,1};
+		int[] inputArray = {0,2,0,1};
+		int[] expectedArray = {0,2,2,3};
+		CountingSort countingSort = new CountingSort(array, 3);
+		
+		int[] countArray = countingSort.countAllValuesTogether(inputArray);
+		
+		assertArrayEquals(countArray, expectedArray);
+	}
+	
+	@Test
+	public void countingSortShouldReturnArraySortedNumerically() {
+		int[] array = {3,1,2,5};
+		int[] expectedArray = {1,2,3,5};
+		CountingSort countingSort = new CountingSort(array, 5);
+		
+		int[] sortedArray = countingSort.countingSort();
+		
+		assertArrayEquals(sortedArray, expectedArray);
+
+	}
+}
